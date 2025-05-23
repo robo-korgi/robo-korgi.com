9a1aeef9c7f6c2593accba498f761833b9958932
mark-mcdermott <mark@markmcdermott.io>
2025-05-23 15:56:16 -0500

    Move CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://robo-korgi.com/',
+  site: 'https://robo-korgi.com',
   base: '/',
   integrations: [tailwind(), sitemap()],
+  build: {
+    assets: 'assets',
+    assetsPrefix: '',
+    format: 'directory',
+  }
 });
