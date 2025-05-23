9a4586bf22880b93424410e6ab855ec4d469582e
mark-mcdermott <mark@markmcdermott.io>
2025-05-23 13:56:00 -0500

    Add path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 
-export default defineConfig({})
+export default defineConfig({
+  base: '/robo-korgi.com/', // ← This line is critical
+  integrations: [tailwind(), sitemap()],
+})
