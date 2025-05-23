02aa36b3bd8ba7c1ace2670c8cfa0570be083c4c
mark-mcdermott <mark@markmcdermott.io>
2025-05-22 19:12:08 -0500

    Clean slate

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        russo: ['"Russo One"', 'sans-serif'],
+      }
+    }
+  },
+  plugins: []
+}
