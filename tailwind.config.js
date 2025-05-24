df0f193255b694a375e7c9f6750e843ba79bf6da
mark-mcdermott <mark@markmcdermott.io>
2025-05-23 23:50:28 -0500

    Add typography package

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,33 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         russo: ['"Russo One"', 'sans-serif'],
-      }
-    }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h2: {
+              '@apply block mt-6 text-base font-bold mb-3 font-russo uppercase text-[#0d0d0d]': {},
+            },
+            p: {
+              '@apply block text-base': {},
+            },
+            ol: {
+              listStyleType: 'decimal',
+              listStylePosition: 'outside',
+              paddingLeft: '1.5rem',
+              fontSize: '1rem',
+              lineHeight: '1.4',
+            },
+            li: {
+              display: 'list-item', 
+              fontSize: '1rem',
+              paddingTop: '0',
+              paddingBottom: '0',
+            },
+          },
+        },
+      },
+    },
   },
-  plugins: []
+   plugins: [require('@tailwindcss/typography')],
 }
